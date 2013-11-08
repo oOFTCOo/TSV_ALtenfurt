3c25cdfaa7d4287ab71d23d0bc9c8228219f8cf9
oOFTCOo <Michael.Schrage@nefkom.ino>
2013-11-08 11:47:31 +0100

    beta 1.2
    neue dinge

---
+++ TSV_Altenfurt/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html manifest="cache.manifest">-->
+<head>
+<script type="text/javascript">
+
+aDivArray = new Array("Spielberichte","Spielberichte2","Spielberichte3","Spielberichte4");
+var iIdx=0;
+
+function fZurueck () {
+iIdx--;
+if(iIdx<0)
+{
+iIdx=0;
+}
+document.getElementById(aDivArray[iIdx]).style.visibility = "visible";
+fUnvisible(iIdx);
+}
+
+function fUnvisible(iIndex)
+{
+	for (var i = 0; i < 4; i++)
+	{
+		if(i!=iIndex)
+		{
+		document.getElementById(aDivArray[i]).style.visibility = "hidden";
+		}
+	}
+}
+
+
+
+function fWeiter () {
+
+iIdx++;
+if(iIdx>3)
+{
+iIdx=3;
+}
+document.getElementById(aDivArray[iIdx]).style.visibility = "visible";
+fUnvisible(iIdx);
+ //document.getElementById("Spielberichte").innerText = aTextArray[iZ];
+ 
+}
+
+
+</script>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>TSV Altenfurt</title>
+	<link rel="stylesheet" href="css/reset.css" type="text/css">
+    <link rel="stylesheet" href="css/app.css"  type="text/css">
+    <link rel="stylesheet" href="css/skin.css" type="text/css">
+
+
+	
+	
+	
+	
+	
+</head>
+
+<body>
+<div id="Spielberichte1" style="visibility:hidden;height:73.6%;width:76.5%;border:1px solid #ccc;font:18px/28px Comic Sans MS, Serif;overflow:auto;font-weight:bold;position:absolute;left:11%;top:22%"> 
+<?php 
+include("Text1.txt"); 
+?>
+</div>
+<div id="Spielberichte2" style="visibility:hidden;height:73.6%;width:76.5%;border:1px solid #ccc;font:18px/28px Comic Sans MS, Serif;overflow:auto;font-weight:bold;position:absolute;left:11%;top:22%"> 
+<?php 
+include("Text2.txt"); 
+?>
+</div>
+<div id="Spielberichte3" style="visibility:hidden;height:73.6%;width:76.5%;border:1px solid #ccc;font:18px/28px Comic Sans MS, Serif;overflow:auto;font-weight:bold;position:absolute;left:11%;top:22%"> 
+<?php 
+include("Text3.txt"); 
+?>
+</div>
+<div id="Spielberichte4" style="visibility:hidden;height:73.6%;width:76.5%;border:1px solid #ccc;font:18px/28px Comic Sans MS, Serif;overflow:auto;font-weight:bold;position:absolute;left:11%;top:22%"> 
+<?php 
+include("Text4.txt"); 
+?>
+</div>
+
+<div id="Spielberichte" style="visibility:hidden;height:73.6%;width:76.5%;border:1px solid #ccc;font:18px/28px Comic Sans MS, Serif;overflow:auto;font-weight:bold;position:absolute;left:11%;top:22%">
+TSV Lichtenau – TSV Altenfurt 
+Durchaus motiviert reiste das Team um das Trainergespann Wirth/Reinthaler nach Neuendettelsau um beim TSV Lichtenau vorzuspielen. Am frühen Sonntagnachmittag wollte man, bei den auch noch sieglosen Lichtenauern, die ersten Punkte der Saison einfahren um im Abstiegskampf zumindest vorübergehend etwas durchatmen zu können. 
+Unterschätzen wollte „Afu“ seinen Gegner aber keineswegs. Man traf schließlich auf eine Mannschaft die seit mehreren Jahren solide Leistungen in der Bezirksoberliga gezeigt hat und somit zu einer festen Größe dieser Spielklasse geworden ist. Die bisher gezeigten Leistungen des Gastgebers, in der Spielzeit 2013/14, wollte die „Erste“ deshalb nicht überbewerten und zollte somit seinem Gegner auch durchaus großen Respekt. Außerdem konnte der Lichtenauer Coach Roland Nixdorf auf einen großen Kader zurückgreifen, welcher gespickt ist mit vielen jungen und äußerst dynamischen Spielen. 
+Nichtsdestotrotz wollte sich Altenfurt davon unbeeindruckt zeigen und aus einer stabilen Defensive heraus den Sieg einfahren. Auf einen offenen Schlagabtausch wollte sich die Mannschaft dabei keineswegs einlassen. 
+
+Vielversprechend kam „Afu“ dann auch ins Spiel und konnte sogleich seine Ambitionen auf einen Sieg untermauern. Altenfurt schaffte es den Gastgeber zu einigen Fehlern im Angriff zu zwingen und eroberte sich, unterstützt durch den starken Altenfurter Schlussmann Zicha, zahlreiche Bälle welche in schnelle Tore umgemünzt werden konnten (8:5, 9:6). Besonders Michael Schrage zeigte in dieser Phase eine ansprechende Leistung und konnte sich durch einige hervorragende Abwehr- und Angriffsaktionen auszeichnen. 
+Leider konnte der Drei-Tore-Vorsprung aber nicht gehalten werden, da sich wieder einmal zu viele Unkonzentriertheiten ins Altenfurter Spiel schlichen. Gerade in der Schlussphase der ersten Halbzeit versagten vor dem gegnerischen Tor wieder die Nerven und es kam zur Anhäufung von Passungenauigkeiten und technischen Fehlern im Angriffsspiel der Altenfurter. So musste sich die Mannschaft mit einem 11:11 beim Gang in die Kabine begnügen. 
+Keineswegs unzufrieden fiel dann aber die Halbzeitanalyse aus, denn es war zu bemerken, dass Lichtenau durchaus zu schlagen war, wenn man es schafft seine Fehlerquote zu minimieren. Weiterhin wurde die Marschroute also klar Richtung Sieg gestellt um die Punkte mit ins heimische Altenfurt nehmen zu können. 
+
+Lichtenau schien aber frischer aus der Kabine zu kommen und konnte sogleich in Führung gehen (11:12). Zunächst zeigte sich „Afu“ aber völlig unbeeindruckt und ließ die Hausherren nicht davonziehen (13:13, 14:14, 16:17). 
+Doch dann kam es - aufgrund der hohen Fehlerquote - wie es kommen musste. Lichtenau zog Tor um Tor davon weil es nun so schien als würde eine völlig verunsicherte Altenfurter Mannschaft auf dem Feld stehen (16:22, 18:24, 19:25, 19:27). Ein technischer Fehler folgte dem Nächsten und am (starken) gegnerischen Torwart scheiterte man teils kläglich. 
+Auch in der Defensive bekam die Mannschaft kaum noch Zugriff auf das Spiel und es deuteten sich erste konditionelle Probleme an. Ein ums andere Mal konnten die Hausherren frei von sechs Meter abschließen und auch die Altenfurter Keeper bekamen kaum noch eine Hand an den Ball. 
+Fünf Minuten vor Spielende zeigte sich Altenfurt dann nochmals kämpferisch und konnte durch einen 5:1-Lauf die Niederlage noch einigermaßen erträglich gestalten. Die Schlusssirene ertönte dann beim Spielstand von 24:28 aus Altenfurter Sicht. 
+
+Alles in Allem kann die „Erste“ mit ihrer gezeigten Leistung keineswegs zufrieden sein. Sicherlich war dies die bisher schwächste Leistung in dieser Saison (trotz der deutlicheren Niederlage gegen Zirndorf). 
+Zwar zeigte man vor allem im ersten Spielabschnitt gute Ansätze und stand im Abwehrverbund weitestgehend stabil und dennoch muss dies über die gesamte Spielzeit konstant abgerufen werden. 
+26 (!) Fehlwürfe, 11 (!) technische Fehler und vier Passfehler sprechen eine deutlich Sprache und zeigen auf warum die Mannschaft bisher ohne Punktgewinn dasteht. Gute Ansätze allein genügen also nicht um in der BOL punkten zu können. 
+
+Es spielten: 
+Deiters, Zicha (beide Tor), Behr, Biehler, Ditze, Güßregen, Kretzschmar, Leischner, Reichert, Schellkopf, Schrage, Spiegel 
+
+TSV Lichtenau – TSV Altenfurt 28:24 
+
+Ausblick 
+Viel Zeit den bis dato verlorenen Punkten nachzutrauern bleibt der „Ersten“ aber natürlich nicht, denn schon am kommenden Samstagabend gastiert in heimischer Halle die Mannschaft des HG/HSC Fürth. Die Fürther stellen aktuell die beste Abwehr der Liga und mussten sich bisher nur knapp den ambitionierten Mannschaften aus Wendelstein und Ansbach geschlagen geben. 
+Gegen einen körperlich robusten Gegner muss sich „Afu“ dann endlich wieder auf seine eigenen Stärken berufen und absolut hochkonzentriert agieren. Gegen das Abwehrbollwerk der Fürther wird man nicht viele Torchancen bekommen, deshalb müssen die wenigen Gelegenheiten die sich bieten konsequent genutzt werden. 
+Die Zuschauer am Samstag um 18:30 Uhr werden aber sicher eine veränderte, hochmotivierte und angriffslustige Altenfurter Mannschaft sehen, die alles daran setzen wird die Punkte in Altenfurt zu behalten. 
+
+</div>
+<a style="text-decoration:none;height:20%;width:11%;color:black;font:300% Arial, Serif;overflow:auto;font-weight:bold;position:absolute;left:0%;top:50%" href="javascript:fZurueck()"></a>	
+<a style="text-decoration:none;height:20%;width:13%;color:black;font:300% Arial, Serif;overflow:auto;font-weight:bold;position:absolute;left:87.5%;top:50%" href="javascript:fWeiter()"></a>	
+
+
+</body>
+</html>
